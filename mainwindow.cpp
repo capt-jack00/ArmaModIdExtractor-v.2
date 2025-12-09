aadea96ea270cafdb833cca8d3a0fb31b7915d00
Tytus <dbednorz@tuta.io>
2025-12-09 10:20:59 +0100

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,60 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QProcess>
+
+//TODO: Add better error handling (Show a message to the user that something went wrong)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->mdpckbtn, &QPushButton::clicked, this, [this]() {
+        QString modpckName = QFileDialog::getOpenFileName(this, "Open File");
+        if (!modpckName.isEmpty()) {
+            input = modpckName;
+            ui->mdpckPath->setText(modpckName);
+        }
+    });
+
+    connect(ui->outbtn, &QPushButton::clicked, this, [this](){
+        QString outfileName = QFileDialog::getOpenFileName(this, "Open File");
+        if(!outfileName.isEmpty()){
+            output = outfileName;
+            ui->outPath->setText(outfileName);
+        }
+    });
+
+    //TODO: Rewiev the connect code below for extract button
+    connect(ui->extractBtn, &QPushButton::clicked, this, [this](){
+        QProcess process;
+        process.start("python3", QStringList() << "extract.py" << "--input" << input << "--output" << output);
+
+        if (!process.waitForFinished()) {
+            qDebug() << "Process timed out or failed to start!";
+            return;
+        }
+
+        QString stdoutOutput = process.readAllStandardOutput();
+        QString stderrOutput = process.readAllStandardError();
+
+        int exitCode = process.exitCode();
+
+        qDebug() << "Exit code:" << exitCode;
+        qDebug() << "STDOUT:" << stdoutOutput;
+        qDebug() << "STDERR:" << stderrOutput;
+
+        if (exitCode == 0) {
+            qDebug() << "Script ran successfully!";
+        } else {
+            qDebug() << "Script failed!";
+        }
+    });
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ mainwindow.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QString>
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QString input;
+    QString output;
+};
